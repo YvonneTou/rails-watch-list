a06462f51b4b73348e1882ea85ce17a2e77a0bc1
Yvonne Tou <tou.yvonne@gmail.com>
2023-01-14 15:04:06 +0900

    create new list completed

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,15 +1,29 @@
 class ListsController < ApplicationController
-  before_action :set_list, only: %w[show create update destroy]
+  before_action :set_list, only: %w[show]
   def index
     @lists = List.all
+    @list = List.new
   end
 
   def show
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to lists_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
 
   def set_list
     @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :lists
+  resources :lists, only: %w[index show create edit update]
 end
