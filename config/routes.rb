2baf60c82c45a06018c40d8d67d85dcc78706c35
Yvonne Tou <tou.yvonne@gmail.com>
2023-01-17 20:22:08 +0900

    change platform on gemfile.lock

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root to: 'lists#index'
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :lists, only: %w[index show create edit update] do
+  resources :lists, only: %w[show create edit update] do
     resources :bookmarks, only: %w[new create]
   end
   resources :bookmarks, only: %w[destroy]
